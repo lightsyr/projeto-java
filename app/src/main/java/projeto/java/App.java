739e8cf9dc178c9dbab01d8b839b470efe94b6ff
Leandro Campos <contato.leandroncampos@gmail.com>
2026-03-16 08:50:12 -0300

    refactor: Removida função de print

--- app/src/main/java/projeto/java/App.java
+++ app/src/main/java/projeto/java/App.java
@@ -8,10 +8,6 @@ public class App {
         return "Hello World!";
     }
 
-    public void print() {
-        System.out.println("Hello World!");
-    }
-
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
